c757e977a6ca667850a745cb2463227873e122c2
Aislan Jefferson <aislanjsb@gmail.com>
2018-09-09 10:49:15 -0300

    Adicionado identificador de prioridade no struct de processo

--- proc.h
+++ proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  char prioridade; // Pode variar de -20 (maior prioridade) a 19 (menor prioridade)
 };
 
 // Process memory is laid out contiguously, low addresses first:
